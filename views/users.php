5caa12ec1870ad02fc25b68bf114c5d0ff52081d
fatimahnazwasi <0110124217@student.nurulfikri.ac.id>
2025-05-29 23:28:08 +0700

    awal

---
+++ views/users.php
@@ -0,0 +1,18 @@
+<section class="container">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div  class="card-body">
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>NO</th>
+                            <th>Name</th>
+                            <th>Telepon</th>
+                            <th>Email</th>
+                            <th>Aksi</th>
+                        </tr>
+                </div>
+            </div>
+        </div>
+    </div> 
+</section> 
